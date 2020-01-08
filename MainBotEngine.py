93293d04fe73443414a3efd8005d08cc0f31861d
tel3port <carn3lian@gmail.com>
2020-01-08 17:49:38 +0300

    almost done

--- MainBotEngine.py
+++ MainBotEngine.py
@@ -8,10 +8,10 @@ import csv
 import globals as gls
 import logging
 import glob
-import sys
 import os
+from random import randint
 
-image_list = glob.glob('/media/*')
+image_list = glob.glob('media/*')
 
 
 class PinterestBot:
@@ -21,6 +21,7 @@ class PinterestBot:
         self.driver = webdriver.Chrome("./chromedriver")
         self. base_url = "https://www.pinterest.com"
         self.login()
+        self.image_list = image_list
 
     def login(self):
         options = webdriver.ChromeOptions()
@@ -35,10 +36,10 @@ class PinterestBot:
         self.driver.find_element_by_name('password').send_keys(self.password)
 
         # click login button
-        self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div/div[3]/form/div[5]/button').click()
-
-    def nav_user(self, user):
-        self.driver.get(f'{self.base_url}/{user}/')
+        try:
+            self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div/div[3]/form/div[5]/button').click()
+        except Exception as e:
+            print("the issue is: ", str(e))
 
     def infinite_scroll(self):
         scroll_pause_time = 3
@@ -65,8 +66,8 @@ class PinterestBot:
         try:
             this_csv = open(my_csv, gls.append)
             csv_writer = csv.writer(this_csv)
-            for single_link in saved_links_list:
-                csv_writer.writerow([str(single_link)])
+            for one_link in saved_links_list:
+                csv_writer.writerow([str(one_link)])
                 print("row (hopefully) written into csv")
 
         except Exception as em:
@@ -79,7 +80,7 @@ class PinterestBot:
             pass
 
     @staticmethod
-    def read_from_csv(my_csv):
+    def read_links_from_csv(my_csv):
         list_of_links = []
         try:
             with open(my_csv, gls.read) as rdr:
@@ -96,37 +97,57 @@ class PinterestBot:
 
         finally:
             print(list_of_links)
-            pass
+            return list_of_links
 
-    def follow_user(self, user):
-        self.nav_user(user)
-        time.sleep(5)
+    @staticmethod
+    def read_descs_from_csv(my_csv):
+        list_of_descs = []
+        try:
+            with open(my_csv, gls.read) as rdr:
+                reader = csv.reader(rdr, delimiter=",")
+                for single_row in reader:
+                    list_of_descs.append(single_row)
+
+        except IOError as x:
+            print("problem reading the user_accounts csv")
+            logging.error('Error occurred ' + str(x))
+        except Exception as e:
+            print("the problem is: ", e)
+            logging.error('Error occurred ' + str(e))
+
+        finally:
+            return list_of_descs
+
+    def follow_user(self, user_link):
 
         button_xpath = "//button[contains(.,'Follow')]"
 
         element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
         element.click()
-        print(f"{user} followed!")
+        print(f"{user_link} followed!")
 
-    def extract_users_from_dialog(self):
-        links_set = set()
-        self.driver.get("https://www.pinterest.com/technews24h/_community/")
-        btn_xpath = '//*[@id="__PWS_ROOT__"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div[2]/button'
+    def extract_users_from_dialog(self, user_link):
+        try:
+            links_set = set()
+            self.driver.get(user_link)
+            btn_xpath = '//*[@id="__PWS_ROOT__"]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[2]/div[1]/div[1]/div/div/div[1]/div/div[2]/button'
 
-        el = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, btn_xpath)))
-        el.click()
+            el = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, btn_xpath)))
+            el.click()
 
-        time.sleep(200)
-        results = self.driver.find_elements_by_xpath('//a[@href]')
+            time.sleep(300)
+            results = self.driver.find_elements_by_xpath('//a[@href]')
 
-        print(f"size of results {len(results)}")
+            print(f"size of results {len(results)}")
 
-        for res in results:
-            final_link = res.get_attribute('href')
-            print(final_link)
-            links_set.add(final_link)
+            for res in results:
+                final_link = res.get_attribute('href')
+                print(final_link)
+                links_set.add(final_link)
 
-        self.append_to_csv(list(links_set), gls.user_accounts_csv)
+            self.append_to_csv(list(links_set), gls.user_accounts_csv)
+        except Exception as e:
+            print("the issue is at ", e)
 
     def pin_image(self):
         self.driver.get(gls.pin_builder)
@@ -142,9 +163,9 @@ class PinterestBot:
         self.driver.find_element_by_xpath(dest_link_xpath).send_keys('https://cool-giveaways.weebly.com/')
         time.sleep(7)
 
-        self.driver.find_element_by_id('media-upload-input').send_keys(os.getcwd()+image_list[0])
+        self.driver.find_element_by_id('media-upload-input').send_keys(os.getcwd()+self.image_list[randint(0, (len(self.image_list)-1))])
 
-        self.driver.find_element_by_xpath(publish_btn_xpath).click() # to reveal the menu
+        self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to reveal the menu
         board = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, board_selector)))
 
         actions = ActionChains(self.driver)
@@ -152,34 +173,18 @@ class PinterestBot:
         time.sleep(3)
         self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to publish the pin
 
-    def follow_and_comment(self):
-        self.driver.get('https://www.pinterest.com/homefeed/')
-        parsed_results_list = []
-
-        home_screen_results = self.driver.find_elements_by_xpath('//a[@href]')
-
-        print(f"size of results {len(home_screen_results)}")
-
-        for res in home_screen_results:
-            try:
-                final_link = res.get_attribute('href')
-                print(final_link)
-                button_xpath = '//*[@id="__PWS_ROOT__"]/div[1]/div[3]/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/button/div'
-
-                element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
-                element.click()
-                print(f"{element} followed!")
-
-            except Exception as e:
-                print("the issue is: ", e)
-
 
 if __name__ == "__main__":
     pn_bot = PinterestBot("testerslimited@gmail.com", "E5XB!D2MerD!XGK")
-    time.sleep(5)
-    pn_bot.extract_users_from_dialog()
-    pn_bot.read_from_csv(gls.user_accounts_csv)
-    pn_bot.pin_image()
+    pn_bot.extract_users_from_dialog("https://www.pinterest.com/technews24h/_community/")
+
+    links_to_follow = pn_bot.read_links_from_csv(gls.user_accounts_csv)
+
+    for single_link in links_to_follow:
+
+        pn_bot.follow_user(single_link)
+
+        pn_bot.pin_image()
 
 
 
