5f567f74e488988839504842fe2e363a737c7657
tel3port <carn3lian@gmail.com>
2020-01-08 19:21:35 +0300

    almost done

--- MainBotEngine.py
+++ MainBotEngine.py
@@ -11,8 +11,6 @@ import glob
 import os
 from random import randint
 
-image_list = glob.glob('media/*')
-
 
 class PinterestBot:
     def __init__(self, username, password):
@@ -21,7 +19,7 @@ class PinterestBot:
         self.driver = webdriver.Chrome("./chromedriver")
         self. base_url = "https://www.pinterest.com"
         self.login()
-        self.image_list = image_list
+        self.login_config = gls.log_file_writer()
 
     def login(self):
         options = webdriver.ChromeOptions()
@@ -39,9 +37,12 @@ class PinterestBot:
         try:
             self.driver.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div/div[3]/form/div[5]/button').click()
         except Exception as e:
-            print("the issue is: ", str(e))
+            logging.error('Login Error occurred ' + str(e))
+            print("the login issue is: ", str(e))
 
     def infinite_scroll(self):
+        self.driver.get('https://www.pinterest.com/homefeed/')
+        count = 0
         scroll_pause_time = 3
 
         # Get scroll height
@@ -59,6 +60,10 @@ class PinterestBot:
             if new_height == last_height:
                 break
             last_height = new_height
+
+            count += 1
+            if count == 12:
+                break
 
     @staticmethod
     def append_to_csv(saved_links_list, my_csv):
@@ -101,30 +106,35 @@ class PinterestBot:
 
     @staticmethod
     def read_descs_from_csv(my_csv):
-        list_of_descs = []
+        list_of_descriptions = []
         try:
             with open(my_csv, gls.read) as rdr:
                 reader = csv.reader(rdr, delimiter=",")
                 for single_row in reader:
-                    list_of_descs.append(single_row)
+                    list_of_descriptions.append(single_row)
 
         except IOError as x:
             print("problem reading the user_accounts csv")
             logging.error('Error occurred ' + str(x))
         except Exception as e:
-            print("the problem is: ", e)
+            print("the problem is: ", str(e))
             logging.error('Error occurred ' + str(e))
 
         finally:
-            return list_of_descs
+            return list_of_descriptions
 
     def follow_user(self, user_link):
+        try:
+            self.driver.get(user_link)
 
-        button_xpath = "//button[contains(.,'Follow')]"
+            button_xpath = "//button[contains(.,'Follow')]"
 
-        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
-        element.click()
-        print(f"{user_link} followed!")
+            element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, button_xpath)))
+            element.click()
+            print(f"{user_link} followed!")
+        except Exception as e:
+            print("problem is at ", str(e))
+            logging.error('Error occurred ' + str(e))
 
     def extract_users_from_dialog(self, user_link):
         try:
@@ -135,7 +145,7 @@ class PinterestBot:
             el = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, btn_xpath)))
             el.click()
 
-            time.sleep(300)
+            time.sleep(600)
             results = self.driver.find_elements_by_xpath('//a[@href]')
 
             print(f"size of results {len(results)}")
@@ -147,44 +157,72 @@ class PinterestBot:
 
             self.append_to_csv(list(links_set), gls.user_accounts_csv)
         except Exception as e:
-            print("the issue is at ", e)
+            print("the issue is at ", str(e))
+            logging.error('Error occurred ' + str(e))
 
-    def pin_image(self):
-        self.driver.get(gls.pin_builder)
-        title_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[1]/textarea'
-        desc_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[3]/div/div[1]/textarea'
-        dest_link_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/textarea'
-        publish_btn_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div/button[2]'
-        board_selector = '//*[@id="__PWS_ROOT__"]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div[2]/div/div[1]/div/div/div/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div'
-        self.driver.find_element_by_xpath(title_xpath).send_keys("2020 be like...")
-        time.sleep(5)
-        self.driver.find_element_by_xpath(desc_xpath).send_keys("winning at life")
-        time.sleep(4)
-        self.driver.find_element_by_xpath(dest_link_xpath).send_keys('https://cool-giveaways.weebly.com/')
-        time.sleep(7)
+    def pin_image(self, single_desc, single_link, single_image):
+        try:
+            self.driver.get(gls.pin_builder)
+            title_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[1]/div/div/div[1]/textarea'
+            desc_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[1]/div[3]/div/div[1]/textarea'
+            dest_link_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/div[1]/textarea'
+            publish_btn_xpath = '/html/body/div[1]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div/button[2]'
+            board_selector = '//*[@id="__PWS_ROOT__"]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div[1]/div/div[2]/div/div/div[2]/div/div[1]/div/div/div/div/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div'
+            self.driver.find_element_by_xpath(title_xpath).send_keys("2020 be like...")
+            time.sleep(5)
+            self.driver.find_element_by_xpath(desc_xpath).send_keys(single_desc)
+            time.sleep(4)
+            self.driver.find_element_by_xpath(dest_link_xpath).send_keys(single_link)
+            time.sleep(7)
 
-        self.driver.find_element_by_id('media-upload-input').send_keys(os.getcwd()+self.image_list[randint(0, (len(self.image_list)-1))])
+            self.driver.find_element_by_id('media-upload-input').send_keys(os.getcwd()+single_image)
 
-        self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to reveal the menu
-        board = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, board_selector)))
+            self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to reveal the menu
+            board = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, board_selector)))
 
-        actions = ActionChains(self.driver)
-        actions.move_to_element(board).click().perform()
-        time.sleep(3)
-        self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to publish the pin
+            actions = ActionChains(self.driver)
+            actions.move_to_element(board).click().perform()
+            time.sleep(3)
+            self.driver.find_element_by_xpath(publish_btn_xpath).click()  # to publish the pin
+
+        except Exception as e:
+            print("problem is at ", str(e))
+            logging.error('Error occurred ' + str(e))
+
+    def kill_browser(self):
+        self.driver.quit()
 
 
 if __name__ == "__main__":
-    pn_bot = PinterestBot("testerslimited@gmail.com", "E5XB!D2MerD!XGK")
-    pn_bot.extract_users_from_dialog("https://www.pinterest.com/technews24h/_community/")
 
-    links_to_follow = pn_bot.read_links_from_csv(gls.user_accounts_csv)
+    while 1:
 
-    for single_link in links_to_follow:
+        pn_bot = PinterestBot("testerslimited@gmail.com", "E5XB!D2MerD!XGK")
+        pn_bot.extract_users_from_dialog(gls.follower_source)
 
-        pn_bot.follow_user(single_link)
+        list_of_landers = ['https://cool-giveaways.weebly.com/',
+                           'https://amzn.to/2Fw2wcz',
+                           'https://amzn.to/36C970V'
+                           ]
+        list_of_descs = pn_bot.read_descs_from_csv(gls.descs_csv)
+        links_to_follow = pn_bot.read_links_from_csv(gls.user_accounts_csv)
+        image_list = glob.glob('media/*')
 
-        pn_bot.pin_image()
+        random_lander = list_of_landers[randint(0, len(list_of_landers)-1)]
+        random_desc = list_of_descs[randint(0, len(list_of_descs)-1)]
+        random_user = links_to_follow[randint(0, len(links_to_follow)-1)]
+        random_image = image_list[randint(0, len(image_list)-1)]
+
+        for _ in range(1000):
+            pn_bot.pin_image(random_desc, random_lander, random_image)
+            time.sleep(randint(3, 30))
+            pn_bot.follow_user(random_user)
+            time.sleep(randint(3, 30))
+
+        pn_bot.infinite_scroll()
+        pn_bot.kill_browser()
+        time.sleep(randint(5, 50))
+
 
 
 
--- globals.py
+++ globals.py
@@ -3,10 +3,12 @@ from random import randint
 import logging
 
 user_accounts_csv = "user_accounts.csv"
+descs_csv = "descs.csv"
 write = 'w'
 read = "r"
 append = 'a'
 pin_builder = 'https://www.pinterest.com/pin-builder/'
+follower_source = 'https://www.pinterest.com/technews24h/_community/'
 
 def sleep_time():
     t = randint(7, 65)
@@ -18,7 +20,7 @@ def sleep_time():
 
 
 def log_file_writer():
-    return logging.basicConfig(filename='errors.log.log',
+    return logging.basicConfig(filename='errors.log',
                                format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
                                datefmt='%Y-%m-%d:%H:%M:%S',
                                level=logging.ERROR)
